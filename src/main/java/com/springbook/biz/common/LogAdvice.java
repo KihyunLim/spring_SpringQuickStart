c7593995e7161abc2e85316289058119c4219c6f
lkh_home <a47165705@gmail.com>
2019-01-06 21:06:46 +0900

    day2 class05 실습

--- src/main/java/com/springbook/biz/common/LogAdvice.java
+++ src/main/java/com/springbook/biz/common/LogAdvice.java
@@ -1,8 +1,17 @@
 package com.springbook.biz.common;
 
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Service;
+
+@Service
 public class LogAdvice {
 
-	public void printLog() {
+	@Pointcut("execution(* com.springbook.biz..*Impl.*(..))")
+	public void allPointcut() {}
+
+	@Before("allPointCut()")
+	public void printLog(){
 		System.out.println("[공통 로그] 비즈니스 로직 수행 전 동작");
 	}
 }
