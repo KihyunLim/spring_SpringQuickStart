95161c5003151002622947eedac73fe550e302c8
lkh_home <a47165705@gmail.com>
2019-01-29 22:40:59 +0900

    day3 class6~7 실습

--- src/main/java/com/springbook/view/board/GetBoardController.java
+++ src/main/java/com/springbook/view/board/GetBoardController.java
@@ -2,16 +2,17 @@ package com.springbook.view.board;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
 public class GetBoardController implements Controller {
 
 	@Override
-	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
 		System.out.println("글 상세조회 처리");
 		
 		// 1. 검색할 게시글 번호 추출
@@ -25,8 +26,9 @@ public class GetBoardController implements Controller {
 		BoardVO board = boardDAO.getBoard(vo);
 		
 		// 3. 응답 화면 구성
-		HttpSession session = request.getSession();
-		session.setAttribute("board", board);
-		return "getBoard";
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("board", board);
+		mav.setViewName("getBoard");
+		return mav;
 	}
 }
