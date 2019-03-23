df58f911fdee3b2df87dde4e0ada5e97766084b2
lkh_home <a47165705@gmail.com>
2019-03-23 18:53:01 +0900

    인터셉터로 로그인 체크 추가

---
+++ src/main/java/com/springbook/biz/common/AuthenticationInterceptor.java
@@ -0,0 +1,29 @@
+package com.springbook.biz.common;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthenticationInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		HttpSession session = request.getSession();
+		Object obj = session.getAttribute("userId");
+		
+		if(obj == null) {
+			response.sendRedirect("login.do");
+			return false;
+		}
+		
+		return true;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+		super.postHandle(request, response, handler, modelAndView);
+	}
+}
